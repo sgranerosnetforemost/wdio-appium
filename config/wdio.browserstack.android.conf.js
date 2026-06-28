209fea38680cabe6721d35b4befcfb669473d2d5
sgranerosnetforemost <santiago.graneros@netforemost.com>
2026-06-28 20:28:58 -0300

    Use direct BrowserStack hub connection

--- config/wdio.browserstack.android.conf.js
+++ config/wdio.browserstack.android.conf.js
@@ -7,26 +7,21 @@ export const config = {
     user: process.env.BROWSERSTACK_USERNAME,
     key: process.env.BROWSERSTACK_ACCESS_KEY,
 
+    protocol: 'https',
     hostname: 'hub.browserstack.com',
+    port: 443,
+    path: '/wd/hub',
 
     specs: [
         '../test/specs/android/delete-note-screen.spec.js'
     ],
 
-    services: [
-        ['browserstack', {
-            app: process.env.BROWSERSTACK_APP_ID,
-            testReporting: true,
-            testReportingOptions: {
-                projectName: 'WDIO Appium Learning',
-                buildName: 'ColorNote Android BrowserStack Build'
-            }
-        }]
-    ],
+    services: [],
 
     capabilities: [{
         platformName: 'Android',
 
+        'appium:app': process.env.BROWSERSTACK_APP_ID,
         'appium:deviceName': 'Samsung Galaxy S23',
         'appium:platformVersion': '13.0',
         'appium:automationName': 'UiAutomator2',
@@ -37,7 +32,10 @@ export const config = {
             buildName: 'ColorNote Android BrowserStack Build',
             sessionName: 'Delete note test',
             debug: true,
-            networkLogs: true
+            networkLogs: true,
+            video: true,
+            deviceLogs: true,
+            appiumLogs: true
         }
     }]
 };
